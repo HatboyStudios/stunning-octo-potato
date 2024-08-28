b8a8f2c866d015a593f29f3d552898127dcc8e0b
NotNotTrojan.jsx <70586375+Idk3333@users.noreply.github.com>
2024-08-28 23:25:51 +0000

    Training with useState for different displays of pages

--- the-app/src/App.js
+++ the-app/src/App.js
@@ -1,13 +1,38 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import Home from './HomeSc.js';
+import Thread from './ThreadSc.js';
+import Settings from './SettingSc.js';
 import './App.css';
 
-import Page from './page.js';
+
 
 function App() {
+  const [page, setPage] = useState('home');
+
+  const switchPage = () => {
+    switch (page) {
+      case 'home':
+        return <Home />;
+      case 'thread':
+        return <Thread />;
+      case 'settings':
+        return <Settings />;
+      default:
+        return <Home />;
+    }
+  };
+
+
 
   return (
     <div>
-      <Page />
+      <nav>
+      <button type="button" onClick={() => setPage('home')}>Home</button>
+      <button type="button" onClick={() => setPage('thread')}>Thread</button>
+      <button type="button" onClick={() => setPage('settings')}>Settings</button>
+      </nav>
+      <div>{switchPage()}</div>
     </div>
   );
 }
